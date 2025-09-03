3da06fc43ccb2f160c5d382e68846492066a084c
sabarimalai_alagar <sabarimalai_alagar>
2025-09-03 13:09:49 +0530

    about us

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { InnerPageHeaderComponent } from '../../components/inner-page-header/inner-page-header.component';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, InnerPageHeaderComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
