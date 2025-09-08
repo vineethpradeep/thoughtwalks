074f2fd8f2bce3ec2c3dc8c0578937b503f23ed4
vineethpradeep <165773642+vineethpradeep@users.noreply.github.com>
2025-09-08 10:05:59 +0100

    added member

--- src/app/components/team/team.component.ts
+++ src/app/components/team/team.component.ts
@@ -1,28 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-team',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SlickCarouselModule],
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.css'],
 })
 export class TeamComponent {
   teamMembers = [
     {
-      name: 'Sabarimalai Alagar',
-      image: 'assets/img/gallery/sabari.png',
-      description: '.Net Full Stack Developer',
-    },
-    {
       name: 'Karthik Elango',
       image: 'assets/img/gallery/karthik.png',
       description: 'Cloud Architect',
     },
     {
-      name: 'Ganesh Premnath',
-      image: 'assets/img/gallery/ganesh.png',
+      name: 'Sabarimalai Alagar',
+      image: 'assets/img/gallery/sabari.png',
+      description: '.Net Full Stack Developer',
+    },
+    {
+      name: 'Raja Manikkam',
+      image: 'assets/img/gallery/raja.png',
+      description: 'Software Architect',
+    },
+    {
+      name: 'Ajith BalaKrishnan',
+      image: 'assets/img/gallery/ajith.png',
       description: '.Net Full Stack Developer',
     },
     {
@@ -30,10 +36,18 @@ export class TeamComponent {
       image: 'assets/img/gallery/vineeth.png',
       description: 'Design Architect',
     },
-    {
-      name: 'Raja Manikkam',
-      image: 'assets/img/gallery/raja.png',
-      description: 'Software Architect',
-    },
   ];
+  slideConfig = {
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: false,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      { breakpoint: 1024, settings: { slidesToShow: 2 } },
+      { breakpoint: 768, settings: { slidesToShow: 1 } },
+    ],
+  };
 }
